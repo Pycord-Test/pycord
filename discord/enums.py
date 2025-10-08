e7279902820d72917a9b72be2b84734871f5c86a
Paillat <paillat@pycord.dev>
2025-10-08 18:22:08 +0200

    :bug: Fix Self import

--- discord/enums.py
+++ discord/enums.py
@@ -27,7 +27,9 @@ from __future__ import annotations
 
 import types
 from enum import Enum as EnumBase
-from typing import Any, Self, TypeVar, Union
+from typing import Any, TypeVar, Union
+
+from typing_extensions import Self
 
 E = TypeVar("E", bound="Enum")
 
